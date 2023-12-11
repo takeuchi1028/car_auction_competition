aa78fd66dca586a11baaad266de7978a923e262f
shuntakeuchi <shun.takeuchi@kikagaku.co.jp>
2023-12-11 21:39:56 +0900

    first commit

--- competition_test.py
+++ competition_test.py
@@ -1,27 +1,22 @@
 import streamlit as st
 import pandas as pd
 
-from sklearn.metrics import recall_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import f1_score
-from sklearn.metrics import accuracy_score
-# AUC
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import r2_score
 
 # Load the data
-bank_test = pd.read_csv('submission_answer.csv')
+df_ans = pd.read_csv('test.csv')
 
 # 目的変数の変数名
-colunm_name = 'result'
-# result 列のfailを0、successを1に変換
-bank_test[colunm_name] = bank_test[colunm_name].map({'fail': 0, 'success': 1})
+colunm_name = 'sellingprice'
 
-st.title('Bankmarketing Competition')
+st.title('Used Car Auction Prices Competition')
 ranking_df = pd.read_csv('ranking.csv')
 
 st.sidebar.title('オプション')
 st.sidebar.write('ランキングボードを並び替えする基準とする評価指標を選択してください。')
-option = ['F1-score', 'Accuracy', 'Recall', 'Precision', 'AUC']
+option = ['平均二乗誤差 (MSE)', '平均絶対誤差 (MAE)', '決定係数 (R2)']
 selected_score = st.sidebar.selectbox('Select your option', option)
 st.sidebar.write('ランキングボードの結果を CSV ファイルで出力したい場合')
 st.subheader('CSV ファイルを提出')
@@ -35,8 +30,7 @@ uploaded_file = st.file_uploader("CSV ファイルをアップロードしてく
 
 # 表のアップロード方法についてサンプルを提示
 st.write('提出する CSV ファイルは以下の形式で提出してください。インデックス列は不要です。')
-st.write('fail は 0, success は 1 に置き換えてください')
-df = pd.DataFrame({'result': [0, 0, 1, 0, 1]})
+df = pd.DataFrame({'sellingprice': [23500, 26400, 13800, 15200]})
 st.dataframe(df)
 
 
@@ -48,26 +42,15 @@ try:
         # 読み込み
         input_df = pd.read_csv(uploaded_file)
 
-        score_acc = round(accuracy_score(bank_test[colunm_name], input_df[colunm_name])*100, 4)
-        score_rec = round(recall_score(bank_test[colunm_name], input_df[colunm_name])*100, 4)
-        score_pre = round(precision_score(bank_test[colunm_name], input_df[colunm_name])*100, 4)
-        score_f1 = round(f1_score(bank_test[colunm_name], input_df[colunm_name])*100, 4)
-        score_auc = round(roc_auc_score(bank_test[colunm_name], input_df[colunm_name])*100, 4)
-
-        # st.write('Accuracy：', score_acc, '%')
-        # st.write('Recall：', score_rec, '%')
-        # st.write('Precision：', score_pre, '%')
-        # st.write('F1score：', score_f1, '%')
-        # st.write('AUC：', score_auc, '%')
-        # 表示オプションを変更
+        score_mse = round(mean_squared_error(df_ans[colunm_name], input_df[colunm_name]), 4)
+        score_mae = round(mean_absolute_error(df_ans[colunm_name], input_df[colunm_name]), 4)
+        score_r2 = round(r2_score(df_ans[colunm_name], input_df[colunm_name]), 4)
 
         df_score = pd.DataFrame({'Name': [name],
                                 'Group': [group_name],
-                                'Accuracy': [score_acc],
-                                'Recall': [score_rec],
-                                'Precision': [score_pre],
-                                'F1-score': [score_f1],
-                                'AUC': [score_auc]})
+                                '平均二乗誤差 (MSE)': [score_mse],
+                                '平均絶対誤差 (MAE)': [score_mae],
+                                '決定係数 (R2)': [score_r2]})
         
         # ranking_df に df_score と同じ行があれば
         if ranking_df[ranking_df['Name'] == name].empty:
@@ -81,11 +64,11 @@ except:
 
 st.sidebar.download_button('csvファイルを出力', ranking_df.to_csv(index=False), 'ranking.csv')
 
-st.sidebar.write('ランキングボードの結果を削除する場合')
-cleared_num = st.sidebar.number_input('消去するインデックスを入力', min_value=0)
-if st.sidebar.button('消去'):
-    ranking_df = ranking_df.drop(index=cleared_num)
-    ranking_df.to_csv('ranking.csv', index=False)
+# st.sidebar.write('ランキングボードの結果を削除する場合')
+# cleared_num = st.sidebar.number_input('消去するインデックスを入力', min_value=0)
+# if st.sidebar.button('消去'):
+#     ranking_df = ranking_df.drop(index=cleared_num)
+#     ranking_df.to_csv('ranking.csv', index=False)
 
 ranking_df = ranking_df.sort_values(selected_score, ascending=False)
 rank = ranking_df[selected_score].rank(method='min', ascending=False).astype(int)
